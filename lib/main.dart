ce3c12af402bed1b04c8e124987b1608cf34eb02
AlirezaGoodarzi8002 <ali.alireza8002@gmail.com>
2024-02-21 11:35:05 +0330

    Fix Android http error

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:world_time/pages/choose_location.dart';
 import 'package:world_time/pages/home.dart';
 import 'package:world_time/pages/loading.dart';
 
+import 'model/my_http_overrides.dart';
+
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(MaterialApp(
     routes: {
       "/": (context) => const Loading(),
